d1b2ee4a2cb27dbdc1014546e2a749fdaf30dbb0
Jaroslav Tyc <mail@jaroslavtyc.com>
2015-09-02 10:54:32 +0200

    Exceptions hierarchy fixed and tested

--- Granam/Scalar/Exceptions/Exception.php
+++ Granam/Scalar/Exceptions/Exception.php
@@ -1,6 +1,7 @@
 <?php
 namespace Granam\Scalar\Exceptions;
 
-interface Exception extends \Granam\Exceptions\Exception {
+interface Exception extends \Granam\Exceptions\Exception
+{
 
 }
--- Granam/Scalar/Exceptions/Runtime.php
+++ Granam/Scalar/Exceptions/Runtime.php
@@ -1,7 +1,7 @@
 <?php
 namespace Granam\Scalar\Exceptions;
 
-class Runtime extends \RuntimeException implements Exception, \Granam\Exceptions\Runtime
+interface Runtime extends Exception, \Granam\Exceptions\Runtime
 {
 
 }
